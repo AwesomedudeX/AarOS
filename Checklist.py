69fc2ccffcc16539b36b317192702a7d4372f969
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2022-07-04 18:01:44 -0600

    Update Checklist.py

--- Checklist.py
+++ Checklist.py
@@ -1,7 +1,7 @@
 def cl():
 
     print("\n\n"+"-"*9+"\nChecklist\n"+"-"*9+"\n")
-    print("Note: Console must be restarted for Checklist changes to be saved - the console will shutdown after this app is closed.")
+    print("Note: Console must be restarted for Checklist changes to be saved.")
     print("\nLoading...\n")
 
     import checklistsource as cls
