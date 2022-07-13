5957c3e26cbd8f39b5ce10ef55a0540c084d56f1
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2022-07-13 14:01:33 -0600

    Bug Fixes

--- Central_Angle_Calculator.py
+++ Central_Angle_Calculator.py
@@ -1,42 +1,46 @@
+
 def cac():
 
     print("\nCentral Angle Calculator (Circle Graphs):")
 
     while True:
 
-        method = input("\nEnter the calculation method you would like to use in here (\'percentage\', \'quantity\' or \'quit\' to quit): ")
+        try:
 
-        if method == "quit" or method == "exit" or method == "q" or method == "close":
-            print("\nApp closed.")
-            break
+            method = input("\nEnter the calculation method you would like to use in here (\'percentage\', \'quantity\' or \'quit\' to quit): ")
 
-        elif method == "percentage" or method == "p" or method == "pct" or method == "P" or method == "Percentage" or method == "PCT" or method == "percent" or method == "Percent":
+            if method == "quit" or method == "exit" or method == "q" or method == "close":
+                print("\nApp Closed.")
+                break
 
-            p = float(input("Enter the percentage in here: "))
-            angle = p*3.6
+            elif method == "percentage" or method == "p" or method == "pct" or method == "P" or method == "Percentage" or method == "PCT" or method == "percent" or method == "Percent":
 
-            print("The central angle for this section is", angle, end="°.\n\n")
+                p = float(input("Enter the percentage in here: "))
+                angle = p*3.6
 
-        elif method == "quantity" or method == "qu" or method == "QU" or method == "Quantity" or method == "qty" or method == "QTY":
+                print("The central angle for this section is", angle, end=" degrees.\n\n")
 
-            quantity = float(input("Enter the quantity of the section in here: "))
-            total = float(input("Enter the total quantity of all sections in here: "))
-            p = quantity/total*100
-            angle = p*3.6
+            elif method == "quantity" or method == "qu" or method == "QU" or method == "Quantity" or method == "qty" or method == "QTY":
 
-            if quantity > total:
-                quantity = input("Quantity is greater than total. Please enter a value less than or equal to the total: ")
+                quantity = float(input("Enter the quantity of the section in here: "))
                 total = float(input("Enter the total quantity of all sections in here: "))
                 p = quantity/total*100
                 angle = p*3.6
 
+                if quantity > total:
+                    quantity = input("Quantity is greater than total. Please enter a value less than or equal to the total: ")
+                    total = float(input("Enter the total quantity of all sections in here: "))
+                    p = quantity/total*100
+                    angle = p*3.6
+
+                else:
+                    print("The central angle for this section is", angle, end=" degrees.\n")
+                    print("This section accounts for", p, end="%")
+                    print(" of the circle graph.")
+
+
             else:
-                print("The central angle for this section is", angle, end="°.\n")
-                print("This section accounts for", p, end="%")
-                print(" of the circle graph.")
+                print("Invalid input. Please try again.")
 
-
-        else:
-            print("Invalid input. Please try again.")
-
-# cac()
+        except:
+            print("\nThere was an error. Please try again.\n")
